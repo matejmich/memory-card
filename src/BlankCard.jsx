c62580cae98d8c7bf1cb48f8bcb2788649fe6fb8
matejmich <matejmich@seznam.cz>
2024-02-16 10:15:19 +0100

    a

--- src/BlankCard.jsx
+++ src/BlankCard.jsx
@@ -1,7 +1,9 @@
 export default function BlankCard() {
     return(
         <div className="card-image">
-            <img src="./src/assets/cover.avif" alt="cover" className="card-image" />
+        <img src="src/assets/cover.avif" alt="cover" className="card-image" />
+
+        
         </div>
     )
 }
